f6e78024268736259014bccbf122134919691e63
Kévin Darcel <kevin.darcel@gmail.com>
2016-10-16 00:32:08 -0500

    Cleanup

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -9,17 +9,23 @@ API_SYNC = 'http://api.mytf1.tf1.fr/mobile/sync/%s?device=%s&key=%s'
 def Start():
 
     ObjectContainer.title1 = NAME
+    ObjectContainer.art = R(ART)
+
+    DirectoryObject.thumb = R(ICON)
+    DirectoryObject.art = R(ART)
+    VideoClipObject.thumb = R(ICON)
+    VideoClipObject.art = R(ART)
+
     HTTP.CacheTime = CACHE_1HOUR
     HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 
 ####################################################################################################
-@handler('/video/mytf1', TITLE, art=ART, thumb=ICON)
+@handler('/video/mytf1', TITLE)
 def MainMenu():
 
     oc = ObjectContainer()
 
     # download the database the first time if doesn't already exists
     database = JSON.ObjectFromURL(API_INIT % ('ios-smartphone'))
-
 
     return oc
