47cad1ae5196775e4722029d691c3fba9f75fc15
Kévin Darcel <kevin.darcel@gmail.com>
2016-10-16 00:04:19 -0500

    Art and icon

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,6 @@
 TITLE = 'MYTF1'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ####################################################################################################
 def Start():
@@ -8,7 +10,7 @@ def Start():
     HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 
 ####################################################################################################
-@handler('/video/mytf1', TITLE)
+@handler('/video/mytf1', TITLE, art=ART, thumb=ICON)
 def MainMenu():
 
     oc = ObjectContainer()
