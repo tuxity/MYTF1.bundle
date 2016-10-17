7f2151ec1630b5f867e10741ca7ea500e793c866
Kévin Darcel <kevin.darcel@gmail.com>
2016-10-16 23:15:34 -0500

    Update flow

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -25,18 +25,45 @@ def Start():
 
 ####################################################################################################
 @handler('/video/mytf1', TITLE)
-def Programs():
+def MainMenu():
 
     oc = ObjectContainer()
 
-    for program in JSON.ObjectFromString(Resource.Load(DB_PROGRAMS)):
-        thumb = 'http://photos1.tf1.fr/image/320/160/%s/%s' % (program['menuImage'], 000000) #last param is a 6 lenght key
+    categories = [{'slug': 'all', 'name': 'Tous les genres'}]
+
+    for prog in JSON.ObjectFromString(Resource.Load(DB_PROGRAMS)):
+        exists = False
+        for cat in categories:
+            if cat['slug'] == prog['categorySlug']:
+                exists = True
+        if exists is False:
+            categories.append({'slug': prog['categorySlug'], 'name': prog['category']})
+
+    for cat in categories:
+        oc.add(DirectoryObject(
+            key = Callback(Programs, filter_by=cat['slug']),
+            title = cat['name']
+        ))
+
+    return oc
+
+@route('/video/mytf1/{filter_by}')
+def Programs(filter_by):
+
+    oc = ObjectContainer()
+
+    for prog in JSON.ObjectFromString(Resource.Load(DB_PROGRAMS)):
+        if filter_by != 'all' and filter_by != prog['categorySlug']:
+            continue
+
+        #thumb = None
+        #if 'menuImage' in prog:
+        #    thumb = 'http://photos1.tf1.fr/image/320/160/%s/%s' % (prog['menuImage'], 000000) #last param is a 6 lenght key
 
         oc.add(DirectoryObject(
-            key = Callback(Videos, program_slug=program['slug']),
-            title = program['title'],
-            summary = program['description'] if 'description' in program else None,
-            thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
+            key = Callback(Videos, program_slug=prog['slug']),
+            title = prog['title'],
+            summary = prog['description'] if 'description' in prog else None
         ))
 
     oc.objects.sort(key=lambda obj: Regex('^The ').split(obj.title)[-1])
@@ -49,9 +76,9 @@ def Videos(program_slug):
 
     program = None
 
-    for p in JSON.ObjectFromString(Resource.load(DB_PROGRAMS)):
-        if p['slug'] is program_slug:
-            program = p
+    for prog in JSON.ObjectFromString(Resource.load(DB_PROGRAMS)):
+        if prog['slug'] is program_slug:
+            program = prog
 
     oc = ObjectContainer(title2=program['title'])
 
