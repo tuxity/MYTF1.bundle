8c5e8c28d6f7c656c6ca4fb49ebfe1d7a3bab075
Twoure <twoure@aol.com>
2017-01-17 23:17:31 -0600

    cleaned service code, and m3u8 resolution parsing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,3 @@
-import re
-
 TITLE = 'MYTF1'
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
@@ -10,6 +8,7 @@ PROGRAMS = '%s/programmes-tv' % BASE_URL
 VIDEOS = '%s/%s/videos'
 
 RE_MEDIA_ID = Regex("c_idwat = '(?P<media_id>[^']+)'")
+
 ####################################################################################################
 def Start():
 
@@ -110,118 +109,23 @@ def Videos(video_cat, prog_url):
         title = video.xpath('./div/div/a/div/p[contains(@class, "title")]/text()')[0]
         summary = video.xpath('./div/div/a/div/p[contains(@class, "stitle")]/text()')[0]
         thumb = 'http:' + img.split(',')[-1].split(' ')[0]
-        duration = video.xpath('./div/div/a/div/p[contains(@class, "uptitle")]/span/text()')[0]
-        originally_available_at = video.xpath('./div/div/a/div/p[contains(@class, "uptitle")]/span/text()')[2]
+        try:
+            duration = int(video.xpath('./div/div/a/div/p[contains(@class, "uptitle")]/span/text()')[0]) * 1000
+        except:
+            duration = None
+
+        try:
+            oaa = Datetime.ParseDate(video.xpath('./div/div/a/div/p[contains(@class, "uptitle")]/span/text()')[2])
+        except:
+            oaa = None
 
         oc.add(VideoClipObject(
-            key=Callback(VideoDetails, title=title, summary=summary, thumb=thumb, duration=duration, originally_available_at=originally_available_at, rating_key=title, url=url),
             title=title,
             summary=summary,
             thumb=thumb,
-            rating_key=title
+            duration=duration,
+            originally_available_at=oaa,
+            url=BASE_URL + url
         ))
 
     return oc
-
-
-@route(PREFIX + '/video/details')
-def VideoDetails(title, summary, thumb, duration, originally_available_at, rating_key, url):
-
-    oc = ObjectContainer()
-
-    playlist_url = GetVideoPlaylistURL(url)
-    streams = GetHLSStreams(playlist_url)
-
-    oc.add(VideoClipObject(
-        key=Callback(VideoDetails, title=title, summary=summary, thumb=thumb, duration=duration, originally_available_at=originally_available_at, rating_key=title, url=url),
-        title=title,
-        summary=summary,
-        thumb=thumb,
-        duration=int(duration) * 1000,
-        originally_available_at=Datetime.ParseDate(originally_available_at).date(),
-        rating_key=rating_key,
-        items=[
-            MediaObject(
-                #protocol=Protocol.HLS,
-                bitrate=stream['bitrate']/1000,
-                audio_channels=2,
-                audio_codec=AudioCodec.AAC,
-                video_codec=VideoCodec.H264,
-                video_resolution=stream['resolution'],
-                #container=Container.MP4,
-                #container=Container.MPEGTS,
-                video_frame_rate=25,
-                optimized_for_streaming=True,
-                parts=[
-                    PartObject(
-                        key=HTTPLiveStreamURL(stream['url'])
-                    )
-                ]
-            ) for stream in streams
-        ]
-    ))
-
-    return oc
-
-
-####################################################################################################
-def GetVideoPlaylistURL(prog_url):
-
-    page = HTTP.Request(BASE_URL + prog_url).content
-    media_id = RE_MEDIA_ID.search(page).group('media_id')
-
-    def GetAuthKey(app_name, media_id):
-
-        secret = String.Decode('VzNtMCMxbUZJ')
-        timestamp = HTTP.Request('http://www.wat.tv/servertime2', cacheTime=60).content
-
-        string = '%s-%s-%s-%s-%s' % (media_id, secret, app_name, secret, timestamp)
-
-        auth_key = Hash.MD5(string) + '/' + timestamp
-        return auth_key
-
-    user_agent = 'MyTF1/16090602 CFNetwork/808.1.4 Darwin/16.1.0'
-    app_name = 'sdk/Iphone/1.0'
-    method = 'getUrl'
-    auth_key = GetAuthKey(app_name, media_id)
-    version = '1.8.14'
-    hosting_application_name = 'com.tf1.applitf1'
-    hosting_application_version = '6.3'
-
-    data = ('appName=%s&method=%s&mediaId=%s&authKey=%s&version=%s&hostingApplicationName=%s&hostingApplicationVersion=%s') % (app_name, method, media_id, auth_key, version, hosting_application_name, hosting_application_version)
-    payload = JSON.ObjectFromString(HTTP.Request('http://api.wat.tv/services/Delivery', headers={'User-Agent': user_agent}, cacheTime=60, data=data).content)
-
-    m3u8_url = re.sub(r'&?bw(?:max|min)=\d+', '', payload['message'])
-    Log.Debug('Playing: ' + m3u8_url)
-
-    return m3u8_url
-
-def GetHLSStreams(url):
-
-    playlist = HTTP.Request(url).content
-    streams = []
-
-    for line in playlist.splitlines():
-        if "BANDWIDTH" in line:
-            stream            = {}
-            stream["bitrate"] = int(Regex('(?<=BANDWIDTH=)[0-9]+').search(line).group(0))
-
-            if "RESOLUTION" in line:
-                stream["resolution"] = int(Regex('(?<=RESOLUTION=)[0-9]+x[0-9]+').search(line).group(0).split("x")[1])
-            else:
-                stream["resolution"] = 0
-
-        elif ".m3u8" in line:
-            path = ''
-
-            if not "audio=" in line or not "video=" in line:
-                break
-
-            if not line.startswith("http"):
-                path = url[ : url.rfind('/') + 1]
-
-            stream["url"] = path + line
-
-            streams.append(stream)
-
-    return sorted(streams, key = lambda stream: stream["bitrate"], reverse = True)
